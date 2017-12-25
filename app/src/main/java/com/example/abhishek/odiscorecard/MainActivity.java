38dd4dd0311f00be2abc839b5e8f0941f2dee1c8
Abhishek <abimano99@gmail.com>
2017-12-26 05:13:38 +0530

    3rd commit

--- app/src/main/java/com/example/abhishek/odiscorecard/MainActivity.java
+++ app/src/main/java/com/example/abhishek/odiscorecard/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.abhishek.odiscorecard;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,11 +26,13 @@ public class MainActivity extends AppCompatActivity {
 
         wktSpinner = new String[]{"1","2","3","4","5","6","7","8","9","10","11","12"};
         Spinner spinner = (Spinner)findViewById(R.id.wkts);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.spinner_item,wktSpinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,wktSpinner);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TextView tv = (TextView) view;
+                tv.setTextColor(Color.WHITE);
 
             }
 
